ceb75c8f4294efd2522f3aabd58fe7c12eee081f
Darken75 <135427240+Darken75@users.noreply.github.com>
2025-06-14 10:00:23 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,6 +2,18 @@ import os, sys, glob, pytz, asyncio, logging, importlib
 from pathlib import Path
 from pyrogram import idle
 
+#Dont Remove My Credit @AV_BOTz_UPDATE 
+#This Repo Is By @BOT_OWNER26 
+# For Any Kind Of Error Ask Us In Support Group @AV_SUPPORT_GROUP
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+logging.getLogger("aiohttp").setLevel(logging.ERROR)
+logging.getLogger("pyrogram").setLevel(logging.ERROR)
+logging.getLogger("aiohttp.web").setLevel(logging.ERROR)
+ 
 from info import *
 from typing import Union, Optional, AsyncGenerator
 from Script import script 
@@ -12,17 +24,14 @@ from web.server import Webavbot
 from utils import temp, ping_server
 from web.server.clients import initialize_clients
 
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-)
-logging.getLogger("aiohttp").setLevel(logging.ERROR)
-logging.getLogger("pyrogram").setLevel(logging.ERROR)
-logging.getLogger("aiohttp.web").setLevel(logging.ERROR)
+#Dont Remove My Credit @AV_BOTz_UPDATE 
+#This Repo Is By @BOT_OWNER26 
+# For Any Kind Of Error Ask Us In Support Group @AV_SUPPORT_GROUP
 
 ppath = "plugins/*.py"
 files = glob.glob(ppath)
 Webavbot.start()
+loop = asyncio.get_event_loop()
 
 async def start():
     print('\n')
@@ -41,6 +50,10 @@ async def start():
             sys.modules["plugins." + plugin_name] = load
             print("Imported => " + plugin_name)
 
+#Dont Remove My Credit @AV_BOTz_UPDATE 
+#This Repo Is By @BOT_OWNER26 
+# For Any Kind Of Error Ask Us In Support Group @AV_SUPPORT_GROUP
+    
     if ON_HEROKU:
         asyncio.create_task(ping_server())
     me = await Webavbot.get_me()
@@ -60,8 +73,12 @@ async def start():
     await web.TCPSite(app, bind_address, PORT).start()
     await idle()
 
+#Dont Remove My Credit @AV_BOTz_UPDATE 
+#This Repo Is By @BOT_OWNER26 
+# For Any Kind Of Error Ask Us In Support Group @AV_SUPPORT_GROUP
+
 if __name__ == '__main__':
     try:
-        asyncio.run(start())  # << Fix applied here
+        loop.run_until_complete(start())
     except KeyboardInterrupt:
         logging.info('----------------------- Service Stopped -----------------------')
